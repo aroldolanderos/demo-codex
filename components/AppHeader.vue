313388549eccbccd42d219439dc90580d4c87791
Aroldo Landeros <aroldolanderos@gmail.com>
2025-07-07 11:54:46 -0400

    feat: add header component with navigation

---
+++ components/AppHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="border-b">
+    <UContainer>
+      <UHorizontalNavigation :links="links" />
+    </UContainer>
+  </div>
+</template>
+
+<script setup lang="ts">
+const links = [
+  { label: 'Option 1', to: '/option1' },
+  { label: 'Option 2', to: '/option2' },
+  { label: 'Option 3', to: '/option3' },
+  { label: 'Option 4', to: '/option4' }
+]
+</script>
